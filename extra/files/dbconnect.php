f5195918a63ab2e7b46dcdebd58fdf523f0cb0a5
elenaver3 <128687994+elenaver3@users.noreply.github.com>
2023-11-23 21:59:11 +0300

    with link

--- extra/files/dbconnect.php
+++ extra/files/dbconnect.php
@@ -1,10 +1,11 @@
 <?php 
-    define('DB_HOST', 'localhost');
-    define('DB_USER', 'root'); 
-    define('DB_PASSWORD', 'root'); 
-    define('DB_NAME', 'shop');
+    define('DB_HOST', 'sql104.byethost17.com');
+    define('DB_USER', 'b17_35486626'); 
+    define('DB_PASSWORD', '0303F0x!'); 
+    define('DB_NAME', 'b17_35486626_shop1');
     try {
         $mysql = new mysqli(DB_HOST,DB_USER,DB_PASSWORD,DB_NAME);
+        $mysql->set_charset('utf8');
     }
     catch (Exception $e) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
